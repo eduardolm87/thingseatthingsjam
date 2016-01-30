b0fd9732599ef4d0d022a3f23002edecf4810c0b
eduardolm <edward.lm87@gmail.com>
2016-01-30 21:25:16 +0000

    jump mode

--- Things Eat Things/Assets/Scripts/GameCam.cs
+++ Things Eat Things/Assets/Scripts/GameCam.cs
@@ -1,34 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameCam : MonoBehaviour 
+public class GameCam : MonoBehaviour
 {
-	public float camSpeed;
-	Vector3 targetLookPos;
-	public float dist;
+    public float camSpeed;
+    Vector3 targetLookPos;
+    public float dist;
 
 
     void Start()
     {
         //Vector3 playerPos = new Vector3()
-
         //todo: Move instantly to the player's pos
     }
 
-	void FixedUpdate()
-	{
-		Vector3 offset = new Vector3( 0, Mathf.Sin( -Mathf.Deg2Rad*Globals.gCamTilt ), Mathf.Cos( -Mathf.Deg2Rad*Globals.gCamTilt ) );
-		offset *= dist;
-		
-		Vector3 targetPos = targetLookPos - offset;
-		float interp = Mathf.Clamp( Time.deltaTime * camSpeed, 0, 1 );
-		Vector3 nuPos = transform.position * (1.0f-interp) + targetPos * (interp);
-		transform.position = nuPos;
-	}
+    void FixedUpdate()
+    {
+        Vector3 offset = new Vector3(0, Mathf.Sin(-Mathf.Deg2Rad * Globals.gCamTilt), Mathf.Cos(-Mathf.Deg2Rad * Globals.gCamTilt));
+        offset *= dist;
+
+        Vector3 targetPos = targetLookPos - offset;
+        float interp = Mathf.Clamp(Time.deltaTime * camSpeed, 0, 1);
+        Vector3 nuPos = transform.position * (1.0f - interp) + targetPos * (interp);
+        transform.position = nuPos;
+    }
 
 
-	public void LookAtThis( Vector3 inPos ) {
-		targetLookPos = inPos;
-	}
+    public void LookAtThis(Vector3 inPos)
+    {
+        targetLookPos = inPos;
+    }
 
 }
