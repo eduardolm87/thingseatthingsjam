5600acbe3c4daaf01becba45cc584623f48213eb
eduardolm <edward.lm87@gmail.com>
2016-01-30 18:45:59 +0000

    Scene

---
+++ Things Eat Things/Assets/Scripts/Hitbox.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hitbox : MonoBehaviour 
+{
+    [SerializeField]
+    Rigidbody Rigidbody;
+
+    [SerializeField]
+    SphereCollider Collider;
+
+}
