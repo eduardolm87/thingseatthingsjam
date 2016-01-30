ef39ae2d2da2fa996a5d3ae24636c2be9d3d50ab
eduardolm <edward.lm87@gmail.com>
2016-01-30 14:56:08 +0000

    Starting UI work

---
+++ Things Eat Things/Assets/BranchesAnimation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BranchesAnimation : MonoBehaviour
+{
+    public float Time = 5;
+
+    void Start()
+    {
+        iTween.ScaleFrom(gameObject, iTween.Hash("scale", Vector3.one * 1.05f, "time", Time, "easetype", iTween.EaseType.easeInQuad, "looptype", iTween.LoopType.pingPong));
+    }
+
+}
