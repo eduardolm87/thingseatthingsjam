bfc8147572c5f845579b31af7dab47bc5208c81a
Ben Mitchell <wilde.eep@gmail.com>
2016-01-31 13:36:52 +0000

    bird sounds and change form effect

---
+++ Things Eat Things/Assets/Sounds.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sounds : MonoBehaviour {
+
+	AudioSource sound;
+	public AudioClip[] musics;
+	const int kChangeForm = 1;
+
+	float LastEventTime;
+
+	void Awake () {
+		sound = GetComponent<AudioSource>();
+	}
+	
+
+
+	void Update () {
+	
+	}
+
+	public void Event_WolfAppears()
+	{
+	}
+
+	public void Event_HunterAppears()
+	{
+	}
+
+	public void Event_GameStarts()
+	{
+//		sound.clip;
+	}
+
+	public void Event_ChangeForm()
+	{
+		sound.PlayOneShot( musics[ kChangeForm ]);
+	}
+
+	
+	
+}
