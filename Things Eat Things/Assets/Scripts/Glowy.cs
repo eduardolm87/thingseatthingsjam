2b907412ffe081d6ca26ec7a09dc1fae44522230
Ben Mitchell <wilde.eep@gmail.com>
2016-01-31 13:07:14 +0000

    tiny light

---
+++ Things Eat Things/Assets/Scripts/Glowy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glowy : MonoBehaviour {
+
+	public float Frequency;
+
+	public SpriteRenderer backSprite;
+	public SpriteRenderer frontSprite;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float fade = .5f + .3f * Mathf.Sin( Time.time * Frequency );
+		
+		backSprite.color = new Color( 1, 1, 1, fade );
+		frontSprite.color = new Color( 1,1,1, 1.0f - fade );
+	}
+}
